b9e2f8ed17b6c974ee29b07245f8bf1d6306453d
tg1625 <35944491+tg1625@users.noreply.github.com>
2020-03-04 11:20:33 -0500

    add data
    Start using api data in Home.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import './App.css'
 
 function App() {
   return (
-    <div class="siteWrapper">
+    <div className="siteWrapper">
       <Header />
       <Home />
     </div>
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,27 +1,68 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 //API Key
 const defaultKey = "3170d73852c5601a02c180337486d21c";
 
 function Home(){
+
+    const [weatherData, setWeatherData] = useState({});
+    const [city, setCity] = useState("Arcahaie");
+    const [currentTemp, setCurrentTemperature] = useState("");
+    const [highTemp, setHighTemperature] = useState("");
+    const [lowTemp, setLowTemperature] = useState("");
+    const [icon, setIcon] = useState("");
+    const [wind, setWind] = useState("");
+    const [humidity,setHumidity] = useState("");
+
+    //get API data 
+    useEffect(() =>{
+        axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${"imperial"}&appid=${defaultKey}`
+            )
+        .then(function(response){
+            setWeatherData(response.data);
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+    }, []);
+
+    //set temperature data
+    useEffect(() => {
+        if (weatherData.main){
+            setCurrentTemperature(weatherData.main.temp);
+            setHighTemperature(weatherData.main.temp_max);
+            setLowTemperature(weatherData.main.temp_min);
+            setIcon(weatherData.weather[0].icon);
+            setHumidity(weatherData.main.humidity);
+            console.log(icon);
+        }
+        if (weatherData.wind){
+            setWind(weatherData.wind.speed);
+        }
+    }, [weatherData]);
+
+    console.log("weatherData", weatherData);
+
     return(
         <div className="home">
-            <h1>Weather in "City"</h1>
+            <h1>Weather in {city}</h1>
 
             <div className="weatherInfo">
                 <div className="weatherInfo_Image">
-                    <img src=""/>
+                    <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`}/>
                 </div>
                 <div className="weatherInfo_Data">
                     <div className="currentTemperature">
-                        <p>00&#176;</p>
+                        <p>{currentTemp}&#176;</p>
                     </div>
                     <div className="otherTemperature">
-                        <p>High temp: <strong>00&#176;</strong></p>
-                        <p>Low temp: <strong>00&#176;</strong></p>
+                        <p>High temp: <strong>{highTemp}&#176;</strong></p>
+                        <p>Low temp: <strong>{lowTemp}&#176;</strong></p>
                     </div>
-                    <p>Humidity: 00%</p>
-                    <p>Wind: 00mph</p>
+                    <p>Humidity: {humidity}%</p>
+                    <p>Wind: {wind}mph</p>
                 </div>
             </div>
 
